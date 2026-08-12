595fccc06836bdf17715d1cb62d38f68e5016f2c
LyHaiAnh <lyhaianh16@gmail.com>
2026-08-12 17:55:44 +0700

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'onlinecourse'
 
 urlpatterns = [
-    path('<int:course_id>/', views.course_details, name='course_details'),
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 ]
